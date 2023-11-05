9de58976799e18675732c5d6051ca10f2967e1d9
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-11-05 11:58:22 +0530

    d

--- src/Components/MidBoard.jsx
+++ src/Components/MidBoard.jsx
@@ -5,7 +5,7 @@ const MidBoard = () => {
   return (
     <div >
     <img style={{marginTop:"160px"}} src={capture}/>
-<img style={{marginTop:"130px",width:"100%",height:"250px"}} src="https://cdn.dribbble.com/users/7162/screenshots/1888419/open-uri20150119-12-2b4861"/>
+{/* <img style={{marginTop:"130px",width:"100%",height:"250px"}} src="https://cdn.dribbble.com/users/7162/screenshots/1888419/open-uri20150119-12-2b4861"/> */}
 
     </div>
   )
